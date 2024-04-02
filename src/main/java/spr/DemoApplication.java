464d8ffc1cb0873cf35e6c6e5144b17eca8e6424
ivanv-lab <ivanvozisov33@yandex.ru>
2024-04-02 11:47:23 +0300

    first commit

---
+++ src/main/java/spr/DemoApplication.java
@@ -0,0 +1,27 @@
+package spr;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.NoSuchElementException;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+
+    @Bean
+    CommandLineRunner runner(PersonRepository repository){
+        return args -> {
+            Person person=new Person();
+            person.setName("John");
+
+            repository.save(person);
+            Person saved=repository.findById(person.getId())
+                    .orElseThrow(NoSuchElementException::new);
+        };
+    }
+}
---
+++ src/main/java/spr/PersonRepository.java
@@ -0,0 +1,10 @@
+package spr;
+
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
+
+public interface PersonRepository extends Repository<Person,Long> {
+    Person save(Person person);
+    Optional<Person> findById(long id);
+}
